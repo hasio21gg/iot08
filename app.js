3b39fb9a01917354aefb29d9687ec264c5eaf720
Hideo Hashimoto <h-hasimoto@st-grp.co.jp>
2017-09-21 14:05:50 +0900

    とりあえず

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('front'));
+app.use(bodyParser.json());
+app.listen(3000);
+
+//
+app.get("/api/users", function(req, res){
+	res.send("**************:");
+});
